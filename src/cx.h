3e7ca835a0189a5440ef734ce378844c0de5329b
rldbs0191 <qkdrldbs01912@gmail.com>
2025-07-19 17:48:16 +0900

    0719

--- src/cx.h
+++ src/cx.h
@@ -4,25 +4,25 @@
 #include "define.h"
 
 typedef double** CXArray;
+class Solver;  
 
 class CXManage {
 
-friend class Solver;
+//friend class Solver;
 friend class Node;
 
 private:
 	int nRegion;
 	int Group;
+	Solver* SOLVER;
+public:
+	CXManage();
+	~CXManage();
 	CXArray DIFFUSION;
 	CXArray REMOVAL;
 	CXArray SCATTERING;	
 	CXArray FISSION;
 	CXArray CHI;
-	Solver* SOLVER;
-public:
-	CXManage();
-	~CXManage();
-
 	void SetSolver(Solver* s);
 	void ReadCX(istream& ins);
 	void ReadCXTable(istream& ins, int groupIndex);
--- src/define.h
+++ src/define.h
@@ -5,6 +5,7 @@
 #include <fstream>  
 #include <sstream>
 #include <string>  
+#include <cstring>
 #include <iomanip>  
 #include <vector>
 #include <cmath>
